747cff17abc15f6e673e93a4fc6458c426ee4959
bogdanshwartz <lyl_2019@inbox.ru>
2026-02-01 21:47:04 +0300

    users api

--- internal/api/users.py
+++ internal/api/users.py
@@ -1,6 +1,8 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from ..models.users import *
+from ..domain.users import *
+from ..domain.subscriptions import *
+from ..domain.configs import *
 
 async def create_new_user(session: AsyncSession, username: str, invited_by: int, 
                           can_take_test: bool, rank: str):
@@ -26,3 +28,28 @@ async def delete_user(session: AsyncSession, name: str): # name is username in t
 async def change_user(*args):
     pass
 
+
+async def get_user_by_id(session: AsyncSession, id: int):
+    return await session.scalar(
+        select(User).where(User.user_id == id)
+    )
+
+async def get_user_by_id_username(session: AsyncSession, name: int):
+    return await session.scalar(
+        select(User).where(User.username == name)
+    )
+
+async def get_user_by_id_subscription(session: AsyncSession, sub_id: int):
+    result = await session.execute(select(User, Subscriptions)
+        .join(Subscriptions, User.user_id == Subscriptions.user_id )
+        .where(Subscriptions.id == sub_id))
+    return result.scalar_one_or_none()
+
+async def get_user_by_id_config(session: AsyncSession, conf_id: int):
+    result = await session.execite(select(User, Configs)
+        .join(Configs, User.user_id == Configs.user_id)
+        .where(Configs.id == conf_id))
+    return result.scalar_one_or_none()
+
+
+    
